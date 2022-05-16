87db4d075914b9573c75966b5387a924c87fc326
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2022-05-15 23:34:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,16 +10,28 @@ env=$5
 git_repo=$6
 git_branch=$7
 
-mkdir -p /tmp/eks-deployment
-git clone -b ${git_branch} https://$git_user:$git_password@github.com/$git_repo.git /tmp/eks-deployment
-DIR="$( cd "$( dirname "$0" )" && pwd )" && ls -latr
-cd /tmp/eks-deployment/avetta/configs/${env}
-yq -i eval ".image.tag = \"$image_tag\"" ${app_name}/values.yaml
+DIR="$( cd "$( dirname "$0" )" && pwd )" && la -later
+VALUES_FILE=$DIR/avetta/configs/${env}/${app_name}/values.yaml
+old_tag=$(cat $VALUES_FILE | grep tag: | awk '{print $2}')
+
+if [[ -z $old_tag ]]; then
+echo 'tag is null! trying to update tag'
+sed -i "s/tag:/tag: $image_tag/" $VALUES_FILE
+elif [[ $old_tag != $image_tag ]]; then
+echo 'updating tag to `${image_tag}`'
+sed -i "s/$old_tag/$image_tag/" $VALUES_FILE
+else
+echo "Nothing to update"
+exit 0
+fi
+
+#yq -i eval ".image.tag = \"$image_tag\"" $VALUES_FILE
+git remote set-url origin https://github.com/avetta/argocd-helm.git
 git config user.email "$git_user"
-git config user.name "$git_user"
+git config user.name "$git_user@avetta.com"
 git add .
 git commit -m "Image tag in ${app_name}/values.yaml for ${app_name} with $image_tag"
-git push
+git push -u origin originize_repo
 echo "Updated image tag in ${app_name}/values.yaml for ${app_name}"
 
 
