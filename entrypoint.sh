4b9fb589c21b5657cfdd7828203d5d7822a6aab9
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2022-05-15 22:27:22 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,15 @@ git_branch=$7
 
 mkdir -p /tmp/eks-deployment
 git clone -b ${git_branch} https://$git_user:$git_password@github.com/$git_repo /tmp/eks-deployment
-cd /tmp/eks-deployment/avetta/config
-yq -i eval ".image.tag = \"$image_tag\"" ${app_name}/${env}-values.yaml
+cd /tmp/eks-deployment/avetta/config/${env}
+yq -i eval ".image.tag = \"$image_tag\"" ${app_name}/values.yaml
 git config user.email "$git_user"
 git config user.name "$git_user"
 git add .
-git commit -m "Image tag in ${app_name}/${env}-values.yaml for ${app_name} with $image_tag"
+git commit -m "Image tag in ${app_name}/values.yaml for ${app_name} with $image_tag"
 git push
-echo "Updated image tag in ${app_name}/${env}-values.yaml for ${app_name}"
+echo "Updated image tag in ${app_name}/values.yaml for ${app_name}"
 
 
-status="Updated image tag in ${app_name}/${env}-values.yaml for ${app_name}"
+status="Updated image tag in ${app_name}/values.yaml for ${app_name}"
 echo "::set-output name=status::$status"
